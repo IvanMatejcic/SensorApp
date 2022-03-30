768d68c7d41e203798f3ae2c4446ec371d9b9ed4
yessirskiii <ivanmatejcic1@gmail.com>
2022-03-31 01:21:56 +0200

    First commit.

--- app/home/home-view-model.js
+++ app/home/home-view-model.js
@@ -1,9 +1,20 @@
-import { fromObject } from '@nativescript/core'
+import { fromObject, Observable } from '@nativescript/core'
+import { startAccelerometerUpdates } from "nativescript-accelerometer"
 
 export function HomeViewModel() {
-  const viewModel = fromObject({
-    /* Add your view model properties here */
-  })
+  const viewModel = new Observable()
+  viewModel.x = 10
+  viewModel.y = 20
+  viewModel.z = 30
+
+  //pri svakoj promjeni položaja emulatora/uređaja poziva se dolje definirana 
+  //funkcija s pomoću koje prikazujemo te promjene na ekranu 
+  var accelerometer = require("nativescript-accelerometer")
+  accelerometer.startAccelerometerUpdates(function(data) {
+    viewModel.set('x', "X = " + data.x.toFixed(3))
+    viewModel.set('y', "Y = " + data.y.toFixed(3))
+    viewModel.set('z', "Z = " + data.z.toFixed(3))
+  }, { sensorDelay: "ui" })
 
   return viewModel
 }
